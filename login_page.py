438399b7fa370f40a10c70829d8d4e8e9f1bd56a
kirthikanna <150948986+kirthikanna@users.noreply.github.com>
2025-04-16 09:49:54 +0100

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -8,14 +8,18 @@ class LoginPage(BasePage):
     EMAIL_FIELD = (By.XPATH,"//input[@type='email']")
     PASSWORD_FIELD = (By.XPATH,"//input[@type='password']")
     SUBMIT_BUTTON = (By.XPATH,"(//a[text()='Login'])[1]")
-    DROP_DOWN = (By.XPATH,"dropdown_title")
+    DROP_DOWN = (By.ID,"dropdown_title")
     SIGN_OUT_BUTTON = (By.XPATH,"//div[text()='Sign Out']")
     ERROR_MESSAGE = (By.XPATH,"(//div[text()='Incorrect Email or Password'])[1]")
 
-    def login(self,email,password):
-        WebDriverWait(self.driver,10).until(EC.presence_of_element_located(self.EMAIL_FIELD)).send_keys(email)
-        WebDriverWait(self.driver,10).until(EC.presence_of_element_located(self.PASSWORD_FIELD)).send_keys(password)
-        WebDriverWait(self.driver,10).until(EC.element_to_be_clickable(self.SUBMIT_BUTTON)).click()
+    def enter_email(self, email):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.EMAIL_FIELD)).send_keys(email)
+
+    def enter_password(self, password):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.PASSWORD_FIELD)).send_keys(password)
+
+    def click_login_button(self):
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.SUBMIT_BUTTON)).click()
 
     def click_signout(self):
         WebDriverWait(self.driver,15).until(EC.element_to_be_clickable(self.SIGN_OUT_BUTTON)).click()
